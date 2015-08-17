5342cdd31f2d0eec0ae69daa2d97e97bb61c538a
Hans Baier <hansfbaier@googlemail.com>
2015-08-17 14:47:31 +0700

    enable SWD debug port

--- Src/stm32f1xx_hal_msp.c
+++ Src/stm32f1xx_hal_msp.c
@@ -55,9 +55,9 @@ void HAL_MspInit(void)
 /* SysTick_IRQn interrupt configuration */
   HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
 
-    /**DISABLE: JTAG-DP Disabled and SW-DP Disabled 
+    /**NOJTAG: JTAG-DP Disabled and SW-DP Enabled 
     */
-  __HAL_AFIO_REMAP_SWJ_DISABLE();
+  __HAL_AFIO_REMAP_SWJ_NOJTAG();
 
   /* USER CODE BEGIN MspInit 1 */
 
